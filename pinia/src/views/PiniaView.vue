2a2c1d9c6b1f371a714db49040c2a677481c70ad
webdevyoo <yjk7454@naver.com>
2025-09-11 00:53:37 +0900

    feat: `pinia` - 구현된 pinia 모듈 PiniaView.vue 컴포넌트에 적용

---
+++ pinia/src/views/PiniaView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useCntStore } from '@/store/cnt'
+const cntStore = useCntStore();
+</script>
+
+<template>
+  <h2>방문자 집계: {{ cntStore.cnt }}</h2>
+  <h2>홀짝 판별: {{ cntStore.oddOrEven }}</h2>
+  <button @click="cntStore.inc">방문자 1 증가</button>
+  <button @click="cntStore.dec">방문자 1 감소</button>
+</template>
