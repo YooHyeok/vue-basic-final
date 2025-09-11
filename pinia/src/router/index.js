5f125b5fa16ccfc80dcd61d6c32bd2ee9a6fbf64
webdevyoo <yjk7454@naver.com>
2025-09-12 05:58:35 +0900

    feat: `pinia` - PiniaView.vue 라우터 및 router-link 등록

--- pinia/src/router/index.js
+++ pinia/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import PiniaView from '../views/PiniaView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/pinia',
+      name: 'pinia',
+      component: PiniaView
     },
     {
       path: '/about',
